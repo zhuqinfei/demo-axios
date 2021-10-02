fde6b39b86eabe8c2a6e567f0796db582bbcccf9
zhuqinfei <1586859762@qq.com>
2021-10-02 23:56:33 +0800

    页面的变化在请求后台响应再变化

--- main.js
+++ main.js
@@ -1,18 +1,24 @@
-//interceptors拦截器
-//在真正返回response时使用
-axios.interceptors.response.use(function(response){
+let book={
+    name:'JS高级程序设计',
+    number:2,
+    id:1
+ }
+ //interceptors拦截器
+ //在真正返回response时使用
+ axios.interceptors.response.use(function(response){
     let config=response.config  //config查看响应有啥配置
     let {method,url,data}=config  //data是请求的data
     if(url==='/books/1' && method==='get'){
-      response.data={   //data是响应的data
-         name:'JS高级程序设计',
-         number:2,
-         id:1
-     }
+      response.data=book   //data是响应的data
+    }else if(url==='/books/1' && method==='put'){
+      Object.assign(book,data)
+      response.data=book
     }
      return response
  })
  
+ /*上面是假的后台*/
+
  axios.get('/books/1')
    .then((response)=>{
       let data=response.data
@@ -25,15 +31,27 @@ axios.interceptors.response.use(function(response){
  $('#app').on('click','#addone',function(){
      var oldNumber=$('#number').text() //string
      var newNumber=oldNumber-0+1
-     $('#number').text(newNumber)
+     axios.put('/books/1',{
+       number:newNumber
+     }).then(()=>{
+       $('#number').text(newNumber)
+     })   
  })
  $('#app').on('click','#minusone',function(){
      var oldNumber=$('#number').text() //string
      var newNumber=oldNumber-0-1
-     $('#number').text(newNumber)
+     axios.put('/books/1',{
+       number:newNumber
+     }).then(()=>{
+       $('#number').text(newNumber)
+     }) 
  })
  $('#app').on('click','#reset',function(){
-     $('#number').text(0)
+   axios.put('/books/1',{
+       number:0
+     }).then(()=>{
+        $('#number').text(0)
+     })   
  })
  
  
